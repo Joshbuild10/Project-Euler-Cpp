b22555b0fa1350e8234a269cec36daae0df2b2ff
Joshbuild10 <88260146+Joshbuild10@users.noreply.github.com>
2022-05-22 12:04:32 +0100

    Problem 1

---
+++ p001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+int sumOf3and5(int limit) // Returns the sum of multiples of 3 and 5 below the specified limit
+{
+    int sum = 0;
+    --limit;
+    int threeCount = limit/3;
+    int fiveCount = limit/5;
+    // Uses sum of arithmetic progressions formula to add respective sum of multiples
+    sum += threeCount/2.0 * (3 * (threeCount + 1));
+    sum += fiveCount/2.0 * (5 * (fiveCount + 1));
+    // Subtracts duplicate common multiples
+    int dupCount = limit/15;
+    sum -= dupCount/2.0 * (15 * (dupCount + 1));
+    return sum;
+}
+int main()
+{
+    int limit;
+    std::cout << "Enter the limit of the multiples: \n";
+    std::cin >> limit;
+    std::cout << sumOf3and5(limit) << std::endl;
+    return 0;
+}
